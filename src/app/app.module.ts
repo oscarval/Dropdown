0728130f9a00f1399214a570b0fcb69b51f80550
Alexis Valdez <oscarval@ucm.es>
2018-04-10 10:21:05 +0200

    Initial Commit. New component Dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DropwdownComponent } from './dropwdown/dropwdown.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropwdownComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dropwdown/dropwdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropwdown',
+  templateUrl: './dropwdown.component.html',
+  styleUrls: ['./dropwdown.component.css']
+})
+export class DropwdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
